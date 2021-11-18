2652ebb61776edf09b8119de2920c1b2f571aa51
Fawwaz Anugrah W D <69834655+Wiradhika6051@users.noreply.github.com>
2021-11-18 21:06:30 +0700

    Jika stringnya betul, maka isi string akan diganti string kosong

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ lines = open(nama_file,'r')
 #preprocess
 lines_list = []
 for line in lines:
-    #mengganti new line dengan string kosong
+    #mengganti new line dengan string kosong( Ngaruh ke isi string juga, tapi karena entar isi string dikosongin, mestinya gak ngaruh ke validasi program)
     line  = re.sub('\n','',line)
-    #menghilangkan komentar
+    #menghilangkan komentar 1 baris
     line = re.sub(" *#.*",'',line)
+    #menghilangkan isi semua string valid
+    line = re.sub('".*"','""',line)
     if(line!=''): #jika jadi string kosong maka tidak perlu diappend
         lines_list.append(line)
 print(lines_list)
