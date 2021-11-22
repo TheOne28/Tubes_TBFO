423cbe509d337d441b6c1ba26089d485542c5155
Fawwaz Anugrah W D <69834655+Wiradhika6051@users.noreply.github.com>
2021-11-22 21:29:58 +0700

    Memperbagus konverter ke VAR

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ symbol_dict = {
     r'\>=': " >= ",
     r'(\<<)': " << ",
     r'\>>': " >> ",
-    r'\&': " & ",
+    r'(.*)&=': r"\1& =",
+    r'(.*)& ([^=])': r"\1 & \2",#Kopi untuk aturan lain
     r'\|': " | ",
     r'\^': " ^ ",
     r'\~': " ~ ",
@@ -33,12 +34,23 @@ symbol_dict = {
     r'(.)\,(.)': r"\1 , \2", 
     r'(.)\:': r"\1 : ", 
     r' \=([^=])': r" = \1", 
+    r'\@{1}[^\=]': " @ ",
+    r'\+{1}[^\=]': " + ",
+    r'\-{1}[^\=]': " - ",
+    r'[^\*]\*{1}[^\=\*]': " * ",
+    r'[^\/]\/{1}[^\=\/]': " / ",
+    r'\%[^\=]': " % ",
+    r'//[^=]': " // ",
+    r'\*\*[^=]': " ** ",
 }
 #list berisii symbol di  python
 python_symbols = ('False','class','finally','is','return','None','continue','for','lambda','try','True','def','from','nonlocal','while',
                     'and','del','global','not','with','as','elif','if','or','yield','assert','else','import','pass','break','except','in',
-                    'raise',
+                    'raise','(',')','[',']','{','}','.',',','<=','>=','<<','>>','&','|','^','~','<','>','==','!=',':','@','+','-','*','/',
+                    '%','//','**',
 )
+assignment_operator = ('=','+=','-=','*=','/=','@=','**=','//=','%=')
+brackets = ('[',']','(',')','{','}')
 def preprocess(nama_file):
     #membuka file
     lines = open(nama_file,'r')
@@ -76,16 +88,16 @@ def preprocess(nama_file):
             #handle dot operator
             line = re.sub(r"([a-zA-Z_])(\w+)*(\.)([a-zA-Z_])(\w+)*",r"\1\2 . \4\5",line)
             #Memeriksa variabel(jika ada),kalau valid masukkin ke token kalau gak gak dimasukin
-            for baris in re.findall(r".* \= .*",line):
+            #for baris in re.findall(r".* \= .*",line):
                 #print(re.findall(r".* \= .*",line)
                 #Memeriksa tiap elemen baris yang memenuhi
                 #Menghilangkan = dan isi (), {},serta []
-                baris = re.sub(r'=',"",baris)
-                baris = baris.split()
-                print(baris)
-                for token in baris: #Periksa tiap token
-                    if(token not in python_symbols and True):#Periksa yang gak ada di symbols
-                        pass
+                #baris = re.sub(r'=',"",baris)
+                #baris = baris.split()
+                #print(baris)
+               # for token in baris: #Periksa tiap token
+                    #if(token not in python_symbols and True):#Periksa yang gak ada di symbols
+                        #pass
             #        #cek validitas
             #        if(not re.search(r"[a-zA-Z_][a-zA-Z_0-9]*",token)):#token gak  valid
             #            line = re.sub(token,'',line)
@@ -94,17 +106,102 @@ def preprocess(nama_file):
             #menambahkan token di suatu line ke lines_list
             if(line!=''): #jika jadi string kosong maka tidak perlu diappend
                 line_array = line.split()
+                #for token in line_array:
+                #print(line_array)
+                for operator in assignment_operator:
+                    if operator in line_array: #Memeriksa jika ada assignment
+                        idx_assignment = line_array.index(operator)
+                        #Memproses Assignment
+                        #if(line_array[idx_assignment-1] not in python_symbols and idx_assignment>0):
+                        #    if(isVarValid(line_array[idx_assignment-1])):
+                        #        line_array[idx_assignment-1]='VAR'
+                        #print(line_array)
+                        filtered_list = [line_array[idx_assignment]]
+                        for i in range(idx_assignment-1,-1,-1):
+                            if line_array[i] not in python_symbols and isVarValid(line_array[i]):
+                                line_array[i] = 'VAR'
+                                filtered_list.insert(0,line_array[i])
+                            elif line_array[i] in python_symbols:
+                                filtered_list.insert(0,line_array[i])
+                        for i in range(idx_assignment+1,len(line_array)):
+                            if line_array[i] not in python_symbols and AssignedValue(line_array[i])=='accepted_var':
+                                line_array[i] = 'ASSIGNED_VAR'
+                                filtered_list.append(line_array[i])
+                            elif line_array[i] not in python_symbols and AssignedValue(line_array[i])=='accepted_num':
+                                line_array[i] = 'ASSIGNED_NUM'
+                                filtered_list.append(line_array[i])
+                            elif line_array[i] in python_symbols:
+                                filtered_list.append(line_array[i])
+                        line_array = filtered_list
+                    #if(line_array[idx_assignment+1] not in python_symbols and idx_assignment>0):
+                    #    if(isAssignedValid(line_array[idx_assignment+1])):
+                    #        line_array[idx_assignment+1]='ASSIGNMENT'                     
+                    #if(idx_assignment-2>=1):
+                       # if(line_array[idx_assignment-2]==','): #memproses sintaks buat tukar variabel
+                        #        if(isVarValid(line_array[idx_assignment-3])and isVarValid(line_array[idx_assignment-1])and isVarValid(line_array[idx_assignment+1])and isVarValid(line_array[idx_assignment+3])):
+                         #           line_array[idx_assignment-3] = 'VAR'
+                          #          line_array[idx_assignment-1],line_array[idx_assignment+1],line_array[idx_assignment+3]
                 #line_array = line
+                print(line_array)
                 lines_list.append(line_array)
     return lines_list
 
+def isVarValid(variabel):
+    num=  '0123456789'
+    lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    underscore = '_'
+    valid_for_title = lowercase+uppercase+underscore
+    valid_for_back = valid_for_title+num
+    #current_state= ['start','accepted','rejected']
+    #if(re.search(r'^[A-Za-z_]([A-Za-z0-9_])*',variabel)):
+    current_state = 'start'
+    #Cek huruf pertama
+    for i in range(len(variabel)):
+        if(i==0 and variabel[i] in valid_for_title and current_state!='rejected'):
+            current_state = 'accepted'
+        elif(i>0 and variabel[i] in valid_for_back and current_state!='rejected'):
+            current_state = 'accepted'
+        else:
+            current_state = 'rejected'
+    return current_state == 'accepted'
 
+def AssignedValue(variabel):
+    num=  '0123456789'
+    lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    underscore = '_'
+    dot = '.'
+    valid_for_title = lowercase+uppercase+underscore
+    valid_for_back = valid_for_title+num
+    #current_state= ['start','accepted_num','dot_transition,accepted_var,rejected']
+    current_state = 'start'
+    #Cek huruf pertama
+    for i in range(len(variabel)):
+        if(i==0 and variabel[i] in num and current_state=='start'):
+            current_state = 'accepted_num'
+        elif(i==0 and variabel[i] in valid_for_title and current_state=='start'):
+            current_state = 'accepted_var'
+        elif(i>0 and variabel[i] in valid_for_back and current_state=='accepted_var'):
+            current_state = 'accepted_var'
+        elif(i>0 and variabel[i] in num and current_state=='accepted_num'):
+            current_state = 'accepted_num'
+        elif(i>0 and variabel[i] in dot and current_state=='accepted_num'):
+            current_state = 'dot_transition'
+        elif(i>0 and variabel[i] in num and current_state=='dot_transition'):
+            current_state = 'accepted_num'
+        else:
+            current_state = 'rejected'
+    if(current_state == 'accepted_var' or current_state == 'accepted_num'):
+        return current_state
+    else:
+        return 'rejected'
 
 def main():
     #meminta nama file
     nama_file = sys.argv[1]
     hasil_tokenisasi = preprocess(nama_file)
-    print(hasil_tokenisasi)
+    #print(hasil_tokenisasi)
     CNF = CNFfromFile("grammar.txt")
     with open("cnfResult.txt", "w") as f:
         f.write(str(CNF))
